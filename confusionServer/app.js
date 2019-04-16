80ad64bb154f8cddc60c4c0b2b17fe6a4adae9dc
Rohan2599 <201651044@iiitvadodara.ac.in>
2019-04-16 14:21:17 +0530

    extended restapi server for authentication

--- confusionServer/app.js
+++ confusionServer/app.js
@@ -47,43 +47,25 @@ app.use(session({
 	store: new FileStore()
 }));
 
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
 
 function auth(req,res,next){
 
 	console.log(req.session);
 
 	if(!req.session.user){
-	var authHeader = req.headers.authorization;
-
-	if(authHeader == null){
-
+	
 		var err = new Error('You are not authenticated');
-		res.setHeader('WWW-Authenticate','Basic');
-		err.status=401;
-		return  next(err);
-	}
-
-	var auth = new Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
-
-	var username = auth[0];
-	var password = auth[1];
-
-	if(username=='admin' && password=='password'){
-		req.session.user='admin';
-		next();
-	}
-	else{
-		var err = new Error('You are not authenticated');
-		res.setHeader('WWW-Authenticate','Basic');
 		err.status= 401;
 		return next(err);
-	}
+	
 
 }
 
 else{
 
-	if(req.session.user === 'admin'){
+	if(req.session.user === 'authenticated'){
 		next();
 	}
 
@@ -100,8 +82,7 @@ app.use(auth);
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+
 app.use('/dishes',dishRouter);
 app.use('/promotions',promoRouter);
 app.use('/leaders',leaderRouter);
--- confusionServer/routes/users.js
+++ confusionServer/routes/users.js
@@ -1,9 +1,119 @@
 var express = require('express');
+const bodyParser = require('body-parser');
+const User = require('../models/user.js');
 var router = express.Router();
 
+router.use(bodyParser.json());
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+
+
+router.post('/signUp',function(req,res,next){
+
+	User.findOne({username:req.body.username})
+	.then((user)=>{
+		if(user!=null){
+			var err = new Error('User Already exists');
+			err.status=403;
+			next(err);
+		}
+
+		else{
+				return 	User.create({
+					username:req.body.username,
+					password:req.body.password
+				});
+		}
+	})
+	.then((user)=>{
+		res.statusCode=200;
+		res.setHeader('Content-Type','application/json');
+
+		res.json(user);
+	},(err)=> next(err))
+	.catch((err)=>{
+		next(err);
+	})
+
+})
+
+
+
+router.post('/login',(req,res,next)=>{
+			if(!req.session.user){
+	
+		var authHeader = req.headers.authorization;
+
+		if(authHeader == null){
+
+			var err = new Error('You are not authenticated');
+			res.setHeader('WWW-Authenticate','Basic');
+			err.status=401;
+			return  next(err);
+		}
+
+		var auth = new Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
+
+		var username = auth[0];
+		var password = auth[1];
+
+
+		User.findOne({username:username})
+		.then((user)=>{
+			if(user === null){
+				var err = new Error('User does not exists');
+				err.status=403;
+				return next(err);
+			}
+
+			else if(user.password!=password){
+			var err = new Error('User does not exists');
+				err.status=403;
+				return next(err);
+			}
+
+			else if(user.username===username && user.password===password){
+			req.session.user='authenticated';
+			res.statusCode=200;
+			res.setHeader('Content-Type','text/plain');
+			res.end('You are authenticated');
+		}
+		
+		})
+		.catch((err)=>{
+			next(err);
+		})
+
+
+		
+
+}
+
+else{
+			res.statusCode=200;
+			res.setHeader('Content-Type','text/plain');
+			res.end('You are already authenticated');
+		}
+
+})
+
+router.get('/logout',(req , res , next)=>{
+	if(req.session){
+		req.session.destroy();
+		res.clearCookie('session-id');
+		res.redirect('/');
+
+	}
+	else{
+		var err = new Error('You are not logged in');
+		err.status = 401;
+		next(err);
+
+	}
+})
+
+
 
 module.exports = router;
